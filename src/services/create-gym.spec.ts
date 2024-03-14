606ebcfe8a3239a053e318814db5f45cdac8a596
unknown <ericmadson67@hotmail.com>
2024-03-14 19:49:49 -0300

    gym controllers and tests

--- src/services/create-gym.spec.ts
+++ src/services/create-gym.spec.ts
@@ -1,16 +1,16 @@
 import { beforeEach, describe, expect, it } from "vitest";
 import { RegisterUseCase } from "./register";
 import { InMemoryGymsRepository } from "@/repositories/in-memory/in-memory-gyms-repository";
-import { CreateGymUseCase } from "./create-gym-use-case";
+import { CreateGymUseCase } from "./create-gym";
 
 let gymsRepository: InMemoryGymsRepository;
 let sut: CreateGymUseCase;
 
 describe("Create Gym Use Case", () => {
   beforeEach(() => {
-    gymsRepository = new InMemoryGymsRepository()
-    sut = new CreateGymUseCase(gymsRepository)
-  })
+    gymsRepository = new InMemoryGymsRepository();
+    sut = new CreateGymUseCase(gymsRepository);
+  });
 
   it("should be able to create gym", async () => {
     const { gym } = await sut.execute({
--- src/services/factories/make-create-gym-use-case.ts
+++ src/services/factories/make-create-gym-use-case.ts
@@ -1,5 +1,5 @@
 import { PrismaGymsRepository } from "@/repositories/prisma/prisma-gyms-repository";
-import { CreateGymUseCase } from "../create-gym-use-case";
+import { CreateGymUseCase } from "../create-gym";
 
 export function makeCreateGymUseCase() {
   const gymsRepository = new PrismaGymsRepository();
